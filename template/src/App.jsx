45816d49856a3e986564e4d55e99e35c2189dd0c
saimanideep <saimanideep765@gmail.com>
2025-03-28 13:44:32 +0530

    1

---
+++ template/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>saimanideep</h1>
+      <h2>ponamanda.saimanideep.s89@kalvium.community</h2>
+    </>
+  )
+}
+
+export default App
